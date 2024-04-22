3cc64a1f2935a77c77d3ba7167ab5256587a4b73
Ermal Bujupaj <ermal.bj@gmail.com>
2024-04-22 20:46:38 +0200

    set webview and add app icon

--- Professional Alarm/RootViewController.swift
+++ Professional Alarm/RootViewController.swift
@@ -6,10 +6,43 @@
 //
 
 import UIKit
+import WebKit
 
 class RootViewController: UIViewController {
 
+    var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        setupWebView()
+        if let url = URL(string: "https://tracking.professionalalarm.eu/mobile") {
+            let request = URLRequest(url: url)
+            webView.load(request)
+        }
+        setConstraints()
     }
+    
+    private func setupWebView() {
+        webView = WKWebView(frame: .zero)
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+        view.addSubview(webView)
+    }
+    
+    private func setConstraints() {
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        webView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        webView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        webView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+    }
+}
+
+extension RootViewController: WKNavigationDelegate {
+    
+}
+
+extension RootViewController: WKUIDelegate {
+    
 }
