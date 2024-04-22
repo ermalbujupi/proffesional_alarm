4181732f95d0b8ff98070c4f0d905affc6c962bf
Ermal Bujupaj <ermal.bj@gmail.com>
2024-04-22 20:14:25 +0200

    first commit

---
+++ Professional Alarm/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Professional Alarm
+//
+//  Created by Ermal Bujupaj on 22.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
