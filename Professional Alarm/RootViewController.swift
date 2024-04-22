6d74c582dfda74142cd6837c3d3c31462f3bfe61
Ermal Bujupaj <ermal.bj@gmail.com>
2024-04-22 20:34:16 +0200

    setup root viewcontroller and remove scene delegates

--- Professional Alarm/RootViewController.swift
+++ Professional Alarm/RootViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  RootViewController.swift
 //  Professional Alarm
 //
 //  Created by Ermal Bujupaj on 22.04.24.
@@ -7,13 +7,9 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class RootViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
 }
-
